2f755c77cfa56d6ea197985ff90cef783762a14c
Vrooblevitch <vrooblevitch@gmail.com>
2019-12-08 21:33:28 +0100

    lesson 46 completed

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,41 @@
+import throttle from 'lodash/throttle'
+
+class RevealOnScroll {
+    constructor () {
+        this.itemsToReveal = document.querySelectorAll(".feature-item")
+        this.hideInitially()
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
+        this.events()
+    }
+    events () {
+        window.addEventListener("scroll", this.scrollThrottle)
+    }
+    calcCaller () {
+        console.log("Scroll fuction run")
+            this.itemsToReveal.forEach(el => {
+                if (el.isReveled == false) {
+                    this.calculateIfScrolledTo(el)
+                }
+            })
+    }
+    calculateIfScrolledTo (el) {
+        console.log("Element was calculated")
+        let scrollPercent = (el.getBoundingClientRect().y /window.innerHeight) * 100
+        if (scrollPercent < 75) {
+            el.classList.add("reveal-item--is-visible")
+            el.isReveled = true
+            if (el.isLastItem == true) {
+                window.removeEventListener("scroll", this.scrollThrottle)
+            }
+        }
+    }
+    hideInitially () {
+        this.itemsToReveal.forEach(el => {
+            el.classList.add("reveal-item")
+            el.isReveled = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
+    }
+}
+
+export default RevealOnScroll;
