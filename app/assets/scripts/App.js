83c35dd22f9ec6cae1b5ff12c189037506bef1ae
Vrooblevitch <vrooblevitch@gmail.com>
2020-01-24 22:56:49 +0100

    Finishedthis course.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,31 +1,42 @@
-import '../styles/styles.css'
-import 'lazysizes'
-import MobileMenu from './modules/MobileMenu'
-import RevealOnScroll from './modules/RevealOnScroll'
-import StickyHeader from './modules/StickyHeader'
-import ClientArea from './modules/ClientArea'
+import "../styles/styles.css";
+import "lazysizes";
+import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import StickyHeader from "./modules/StickyHeader";
+import ClientArea from "./modules/ClientArea";
 
-new ClientArea()
-new StickyHeader()
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+import React from "react";
+import ReactDOM from "react-dom";
+import MyAmazingComponent from "./modules/MyAmazingComponent";
+
+ReactDOM.render(
+  <MyAmazingComponent />,
+  document.querySelector("#my-react-example")
+);
+
+new ClientArea();
+new StickyHeader();
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 new MobileMenu();
-let modal
+let modal;
 
 document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
-    e.preventDefault()
+    e.preventDefault();
     if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
-        modal = new x.default()
-        setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("There was a problem."))
+      import(/* webpackChunkName: "modal" */ "./modules/Modal")
+        .then(x => {
+          modal = new x.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        })
+        .catch(() => console.log("There was a problem."));
     } else {
-      modal.openTheModal()
+      modal.openTheModal();
     }
-  })
-})
+  });
+});
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
---
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function MyAmazingComponent() {
+  return (
+    <div>
+      <h1 className="section-title section-title--blue">
+        This Is My Amazing Component
+      </h1>
+      <p>React is great!!!</p>
+    </div>
+  );
+}
+
+export default MyAmazingComponent;
